35fd667d6ce714993a9a297e168ee05f74243a1d
Aleksander Tester <kotycode@onet.pl>
2018-09-26 22:30:12 +0200

    sterowanie diodami6

--- jeden/src/main/java/jeden/jeden/App.java
+++ jeden/src/main/java/jeden/jeden/App.java
@@ -10,7 +10,7 @@ public class App {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		System.out.println("TEST JEDEeeeN JEDEN");
+		System.out.println("TEeeeST JEDEeeeN JEDEN");
 
 		// get GIPO controller
 		final GpioController gpio = GpioFactory.getInstance();
