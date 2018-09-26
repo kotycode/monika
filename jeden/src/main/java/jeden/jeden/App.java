058ca490f8d13e62875d0d1145a533ca376d73eb
Aleksander Tester <kotycode@onet.pl>
2018-09-26 18:30:06 +0200

    sterowanie diodami

--- jeden/src/main/java/jeden/jeden/App.java
+++ jeden/src/main/java/jeden/jeden/App.java
@@ -1,11 +1,69 @@
 package jeden.jeden;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
 public class App {
-	
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws InterruptedException {
+
 		System.out.println("TEST JEDEN JEDEN");
+
+		// get GIPO controller
+		final GpioController gpio = GpioFactory.getInstance();
+
+		// create the pin with parameter PinState.HIGH
+		// will instantly power up the pin
+		final GpioPinDigitalOutput pin02 = gpio.provisionDigitalOutputPin(
+				RaspiPin.GPIO_02, "PinLED", PinState.HIGH);
+		final GpioPinDigitalOutput pin03 = gpio.provisionDigitalOutputPin(
+				RaspiPin.GPIO_03, "PinLED", PinState.HIGH);
+		final GpioPinDigitalOutput pin04 = gpio.provisionDigitalOutputPin(
+				RaspiPin.GPIO_04, "PinLED", PinState.HIGH);
 		
+		System.out.println("Lights ON");
+
+		// wait 2 sec
+		Thread.sleep(2000);
+
+		// turn off GPIO /2/3/4
+		pin02.low();
+		pin03.low();
+		pin04.low();
+		System.out.println("End of light");
+
+		// wait 2 sec
+		Thread.sleep(2000);
+
+		// turn on lights in sequences
+		System.out.println("turn on light in sequences");
+		pin02.high();
+		Thread.sleep(700);
+		pin03.high();
+		Thread.sleep(800);
+		pin04.high();
+
+		// wait 1 sec
+		Thread.sleep(1000);
+
+		pin02.low();
+		pin03.low();
+		pin04.low();
+
+		// wait 1sec
+		Thread.sleep(1000);
+
+		// turn on GPIO /2/3/4 and then turn off
+		System.out.println("Light is on for 3 sec");
+		pin02.pulse(1000, true);
+		pin03.pulse(2000, true);
+		pin04.pulse(3000, true);
+
+		// release the GPIO controller res
+		gpio.shutdown();
 	}
 
 }
