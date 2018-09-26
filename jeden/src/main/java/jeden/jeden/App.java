3afbf4842aa6d2ea64d3721d650b804155e29b86
Aleksander Tester <kotycode@onet.pl>
2018-09-27 00:48:49 +0200

    sterowanie diodami7.4

--- jeden/src/main/java/jeden/jeden/App.java
+++ jeden/src/main/java/jeden/jeden/App.java
@@ -1,7 +1,7 @@
 package jeden.jeden;
 
-import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
